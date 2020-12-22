c66554e8f926dca261cc9b08d7462f5b063890ef
Diego Machaca <diego.machaca@gmail.com>
2020-12-21 23:34:32 -0500

    feat(carga): insert proceso

--- carga/proceso.py
+++ carga/proceso.py
@@ -1,10 +1,23 @@
 import psycopg2
 from connexion import connect_db_target, connect_db_origin
 
-def get_proceso_origin():
-    pass
 def insert_proceso_target():
-    pass
+    try: 
+        con = connect_db_target()
+        cur = con.cursor()
+        data = (' D.S.N.°165-2019-PCM',1,108) # change per 2021 process 
+        cur.execute("INSERT INTO proceso(nombre, estado, jne_idproceso) \
+                    values(%s, %s, %s)", data)
+        con.commit()
+        con.close()
+        print("Proceso inserts success!")
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while fetching data", error)
+
+    finally:
+        if(con):
+            cur.close()
+            con.close()            
 
 if __name__ == "__main__":
     insert_proceso_target()
