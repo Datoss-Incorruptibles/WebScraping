894ca2c036d99bf0692f822a7cfbf5c9a0439389
Angela Haro <aharo@romacl.com>
2021-03-14 21:08:28 -0300

    Se modifica datos de profesión

--- carga/candidato.py
+++ carga/candidato.py
@@ -74,7 +74,7 @@ def update_candidato_target():
             update candidato ca  set profesion = pr.profesion
             from
             (
-            select ca.jne_idcandidato, ca.jne_idhojavida, STRING_AGG (estudio, ' ') profesion
+            select ca.jne_idcandidato, ca.jne_idhojavida, STRING_AGG (estudio, ', ') profesion
             from candidato_estudio ce 
             left join candidato ca on  ca.jne_idhojavida = ce.jne_idhojavida
             where ca.nivel_estudio_id_max in (4) and  ce.nivel_estudio_estado in ('CONCLUIDO')  AND CE.NIVEL_ESTUDIO_ID NOT IN ('1','2')
@@ -84,7 +84,7 @@ def update_candidato_target():
             update candidato ca  set profesion = pr.profesion
             from
             (
-            select ca.jne_idcandidato, ca.jne_idhojavida, STRING_AGG (estudio, ' ') profesion
+            select ca.jne_idcandidato, ca.jne_idhojavida, STRING_AGG (estudio, ', ') profesion
             from candidato_estudio ce 
             left join candidato ca on  ca.jne_idhojavida = ce.jne_idhojavida
             where ca.nivel_estudio_id_max in (3) and  ce.nivel_estudio_estado in ('CONCLUIDO') AND CE.NIVEL_ESTUDIO_ID NOT IN ('1','2')
@@ -137,7 +137,7 @@ def update_candidato_target():
             update candidato ca  set profesion = pr.profesion
             from
             (
-            select ca.jne_idcandidato, ca.jne_idhojavida, STRING_AGG (estudio, ',') profesion
+            select ca.jne_idcandidato, ca.jne_idhojavida, STRING_AGG (estudio, ', ') profesion
             from candidato_estudio ce 
             left join candidato ca on  ca.jne_idhojavida = ce.jne_idhojavida
             where ca.nivel_estudio_id_max in (6) and  ce.nivel_estudio_estado in ('UNIVERSITARIO', 'MAGISTER', 'DOCTOR', 'TITULADO DOCTOR')
@@ -147,13 +147,121 @@ def update_candidato_target():
             update candidato ca  set profesion = pr.profesion
             from
             (
-            select ca.jne_idcandidato, ca.jne_idhojavida, STRING_AGG (estudio, ',') profesion
+            select ca.jne_idcandidato, ca.jne_idhojavida, STRING_AGG (estudio, ', ') profesion
             from candidato_estudio ce 
             left join candidato ca on  ca.jne_idhojavida = ce.jne_idhojavida
             where ca.nivel_estudio_id_max in (5) and  ce.nivel_estudio_estado in ('BACHILLER','UNIVERSITARIO')
             group by ca.jne_idcandidato, ca.jne_idhojavida 
             ) pr where ca.jne_idcandidato = pr.jne_idcandidato and ca.jne_idhojavida = pr.jne_idhojavida;
 
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134133;
+            update candidato set profesion = 'INGENIERO QUIMICO' where jne_idhojavida = 136626;
+            update candidato set profesion = 'ARQUITECTO' where jne_idhojavida = 134382;
+            update candidato set profesion = 'BACHILLER EN CIENCIAS MILITARES' where jne_idhojavida = 137291;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134127;
+            update candidato set profesion = 'INGENIERO INDUSTRIAL, DOCTOR EN ADMINISTRACION ESTRATEGICA DE EMPRESAS' where jne_idhojavida = 134982;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 133886;
+            update candidato set profesion = 'INGENIERO INDUSTRIAL, DOCTOR EN ADMINISTRACION ESTRATEGICA DE EMPRESAS' where jne_idhojavida = 134982;
+            update candidato set profesion = 'INGENIERO CIVIL' where jne_idhojavida = 136570;
+            update candidato set profesion = 'MEDICO CIRUJANO' where jne_idhojavida = 134453;
+            update candidato set profesion = 'TÍTULO PROFESIONAL TÉCNICO EN TÉCNICA EN FARMACIA' where jne_idhojavida =134969;
+            update candidato set profesion = 'ADMINISTRACION DE EMPRESAS, MAESTRÍA  EN ADMINISTRACIÓN DE EMPRESAS' where jne_idhojavida = 133878;
+            update candidato set profesion = 'ABOGADO, MAGÍSTER EN DERECHO PENAL' where jne_idhojavida = 134976;
+            update candidato set profesion = 'ABOGADO, GRADO DE MAESTRO EN DERECHO (GRADO DE MAESTRO)' where jne_idhojavida = 135035;
+            update candidato set profesion = 'LICENCIADO EN ECONOMÍA, MAESTRÍA EN ADMINISTRACIÓN DE NEGOCIOS (MBA)' where jne_idhojavida = 135744;
+            update candidato set profesion = 'ABOGADA, DOCTORA EN GESTION PUBLICA Y GOBERNABILIDAD' where jne_idhojavida = 135407;
+            update candidato set profesion = 'ABOGADO UNIVERSIDAD PRIVADA DE TACNA, MAGISTER EN DERECHO PROCESAL PENAL' where jne_idhojavida = 137817;
+            update candidato set profesion = 'BACHILLER EN CIENCIAS-INGENIERÍA AGRÍCOLA' where jne_idhojavida = 137553;
+            update candidato set profesion = 'BACHILLER EN SOCIOLOGÍA' where jne_idhojavida = 137384;
+            update candidato set profesion = 'CONTADOR PÚBLICO, MAGÍSTER EN DIRECCIÓN DE EMPRESAS' where jne_idhojavida = 134123;
+            update candidato set profesion = 'INGENIERO ECONOMISTA' where jne_idhojavida = 137294;
+            update candidato set profesion = 'SECUNDARIA' where jne_idhojavida = 135714;
+            update candidato set profesion = 'LICENCIADO EN CIENCIAS DE LA COMUNICACIÓN, ABOGADO' where jne_idhojavida = 135443;
+            update candidato set profesion = 'BACHILLER EN INGENIERIA DE MINAS' where jne_idhojavida = 135866;
+            update candidato set profesion = 'LICENCIADA EN ADMINISTRACIÓN' where jne_idhojavida = 134290;
+            update candidato set profesion = 'LICENCIADO EN ADMINISTRACION Y CIENCIAS POLICIALES, ABOGADO, BACHILLER EN EDUCACIÓN' where jne_idhojavida = 133847;
+            update candidato set profesion = 'LICENCIADO EN ADMINISTRACION' where jne_idhojavida = 133873;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134032;
+            update candidato set profesion = 'INGENIERO INDUSTRIAL' where jne_idhojavida = 135417;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 133855;
+            update candidato set profesion = 'CIRUJANO DENTISTA,BACHILLER EN CIENCIAS ADMINISTRATIVAS' where jne_idhojavida = 134371;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134075;
+            update candidato set profesion = 'LICENCIADO EN PERIODISMO' where jne_idhojavida = 133993;
+            update candidato set profesion = 'LICENCIADO EN CIENCIAS DE LA COMUNICACION' where jne_idhojavida = 133916;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134345;
+            update candidato set profesion = 'ENFERMERA' where jne_idhojavida = 134201;
+            update candidato set profesion = 'SECUNDARIA' where jne_idhojavida = 133839;
+            update candidato set profesion = 'ECONOMISTA' where jne_idhojavida = 134375;
+            update candidato set profesion = 'LICENCIADO EN CIENCIAS MILITARES' where jne_idhojavida = 135743;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida =133802;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 133812;
+            update candidato set profesion = 'LICENCIADA EN TURISMO' where jne_idhojavida = 134446;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134148;
+            update candidato set profesion = 'BACHILLER EN CIENCIAS AGRARIAS' where jne_idhojavida = 135596;
+            update candidato set profesion = 'ABOGADA' where jne_idhojavida = 135824;
+            update candidato set profesion = 'CONTADOR PUBLICO' where jne_idhojavida = 135601;
+            update candidato set profesion = 'LICENCIADO EN EDUCACION SECUNDARIA' where jne_idhojavida = 134193;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134683;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134277;
+            update candidato set profesion = 'LICENCIADO EN ADMINISTRACION' where jne_idhojavida = 135713;
+            update candidato set profesion = 'CIRUJANO DENTISTA' where jne_idhojavida = 134636;
+            update candidato set profesion = 'ARQUITECTA' where jne_idhojavida = 134643;
+            update candidato set profesion = 'LICENCIADO EN PERIODISMO' where jne_idhojavida = 133942;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 133879;
+            update candidato set profesion = 'INGENIERO ADMINISTRATIVO' where jne_idhojavida = 135185;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134181;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 136417;
+            update candidato set profesion = 'BACHILLER EN INGENIERÍA DE MINAS' where jne_idhojavida = 136372;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134156;
+            update candidato set profesion = 'LICENCIADO EN EDUCACION, ESPECIALIDAD: AREA PRINCIPAL: LENGUA ESPAÑOLA AREA SECUNDARIA: LITERATURA' where jne_idhojavida = 134325;
+            update candidato set profesion = 'LICENCIADO EN CIENCIAS DE LA COMUNICACIÓN, BACHILLER EN INGENIERIA DE SISTEMAS' where jne_idhojavida = 135696;
+            update candidato set profesion = 'INGENIERO MECÁNICO' where jne_idhojavida = 134067;
+            update candidato set profesion = 'LICENCIADA EN EDUCACIÓN PRIMARIA' where jne_idhojavida = 134078;
+            update candidato set profesion = 'INGENIERO QUIMICO' where jne_idhojavida = 134320;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134197;
+            update candidato set profesion = 'TRABAJADOR SOCIAL' where jne_idhojavida = 133836;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 133752;
+            update candidato set profesion = 'LICENCIADO EN PERIODISMO' where jne_idhojavida = 133954;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134084;
+            update candidato set profesion = 'LICENCIADA EN EDUCACIÓN  INICIAL Y PRIMARIA' where jne_idhojavida = 135058;
+            update candidato set profesion = 'ARQUITECTO' where jne_idhojavida = 134780;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134005;
+            update candidato set profesion = 'BACHILLER EN CIENCAS DE LA COMUNICACIÓN SOCIAL' where jne_idhojavida = 135769;
+            update candidato set profesion = 'TÍTULO PROFESIONAL DE LICENCIADO EN ENFERMERIA,BACHILLER EN ESTOMATOLOGÍA' where jne_idhojavida = 134185;
+            update candidato set profesion = 'CONTADOR PÚBLICO' where jne_idhojavida = 134225;
+            update candidato set profesion = 'INGENIERO AGRONOMO' where jne_idhojavida = 134011;
+            update candidato set profesion = 'LICENCIADA EN CIENCIAS DE LA COMUNICACION' where jne_idhojavida = 134727;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 133891;
+            update candidato set profesion = 'MÉDICO CIRUJANO' where jne_idhojavida = 133917;
+            update candidato set profesion = 'LICENCIADO EN PERIODISMO' where jne_idhojavida = 134125;
+            update candidato set profesion = 'LICENCIADO EN ADMINISTRACION' where jne_idhojavida =134171;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134301;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134301;
+            update candidato set profesion = 'INGENIERA CIVIL' where jne_idhojavida = 134190;
+            update candidato set profesion = 'LICENCIADA EN ENFERMERÍA' where jne_idhojavida = 135958;
+            update candidato set profesion = 'ABOGADO, LICENCIADA EN ADMINISTRACIÓN DE EMPRESAS' where jne_idhojavida = 137381;
+            update candidato set profesion = 'LICENCIADA EN EDUCACION' where jne_idhojavida = 137477;
+            update candidato set profesion = 'TITULO DE SEGUNDA ESPECIALIDAD PROFESIONAL INVESTIGACION Y GESTION EDUCATIVA - UNIVERSITARIO, ABOGADO, LICENCIADO EN EDUCACION ESPECIALIDAD MATEMATICA Y COMPUTACION' where jne_idhojavida = 134929;
+            update candidato set profesion = 'LICENCIADO EN EDUCACIÓN SECUNDARIA' where jne_idhojavida = 135579;
+            update candidato set profesion = 'LICENCIADA EN OBSTETRICIA' where jne_idhojavida = 134672;
+            update candidato set profesion = 'LICENCIADA EN ENFERMERIA' where jne_idhojavida = 133801;
+            update candidato set profesion = 'CONTADOR PÚBLICO' where jne_idhojavida = 133912;
+            update candidato set profesion = 'LICENCIADO EN EDUCACION' where jne_idhojavida = 133783;
+            update candidato set profesion = 'INGENIERO CIVIL' where jne_idhojavida = 134505;
+            update candidato set profesion = 'ARQUITECTO' where jne_idhojavida = 134574;
+            update candidato set profesion = 'MEDICO CIRUJANO' where jne_idhojavida = 134235;
+            update candidato set profesion = 'TITULO PROFESIONAL DE INGENIERO CIVIL' where jne_idhojavida = 134507;
+            update candidato set profesion = 'ABOGADA' where jne_idhojavida = 134260;
+            update candidato set profesion = 'ABOGADO, LICENCIADO EN ADMINISTRACION Y CIENCIAS POLICIALES, BACHILLER EN EDUCACION' where jne_idhojavida = 134270;
+            update candidato set profesion = 'LICENCIADO EN ESTADISTICA' where jne_idhojavida = 133950;
+            update candidato set profesion = 'LICENCIADO EN ENFERMERIA' where jne_idhojavida = 134701;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134019;
+            update candidato set profesion = 'LICENCIADA EN NEGOCIOS INTERNACIONALES' where jne_idhojavida = 136973;
+            update candidato set profesion = 'LICENCIADO EN EDUCACION SECUNDARIA' where jne_idhojavida = 134143;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 134373;
+            update candidato set profesion = 'LICENCIADA EN PSICOLOGÍA HUMANA' where jne_idhojavida = 134484;
+            update candidato set profesion = 'ABOGADO' where jne_idhojavida = 133753;
+            update candidato set profesion = 'LICENCIADO EN ADMINISTRACION' where jne_idhojavida = 133763;
             
          """
         cur.execute(query)
